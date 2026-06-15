df3446f82faf4a30b7a255db9e703c42e6d2a5a2
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-15 09:26:46 +0530

    Time: 2253 ms (5.02%), Space: 23.2 MB (77.35%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n= s.length();
+        vector<int> kmp(n, 0);
+        int i=1, j=0;
+        while(i < n){
+            if(s[i] == s[j]){
+                kmp[i]= j+1;
+                j++;
+            }
+            else{
+                if(j==0) kmp[i]=0;
+                else{
+                    j= kmp[j-1];
+                    continue;
+                }
+            }
+            i++;
+        }
+        for(int i: kmp){
+            cout<<i<<" ";
+        }
+        return s.substr(0,kmp[n-1]);
+    }
+};
