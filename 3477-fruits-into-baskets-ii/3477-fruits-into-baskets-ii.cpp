aa3111f8e003d9797b0a715ae46d940d0e877233
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-29 15:26:19 +0530

    Time: 3 ms (48.95%), Space: 48.9 MB (77.23%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.cpp
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.cpp
@@ -1,61 +1,17 @@
 class Solution {
 public:
-    // struct node{
-    //     int maxi;
-    //     int left, right;
-    //     node(int maxi, int left, int right){
-    //         this->maxi= maxi;
-    //         this->left = left;
-    //         this->right = right;
-    //     }
-    // };
-    class segmentTree{
-        int size;
-        vector<int> nums;
-        vector<int> st;
-        int build (int idx, int left, int right){
-            if(left == right){
-                st[idx]= nums[left];
-                return nums[left];
-            }
-
-            int maxi= INT_MIN;
-            maxi= max(maxi, build(idx*2+1, left, (left + right) /2 ));
-            maxi= max(maxi, build(idx*2+2, (left+right)/2 + 1, right));
-            st[idx]= maxi;
-            return maxi;
-        }
-        public:
-        segmentTree(vector<int>& nums){
-            this->size = nums.size();
-            this->nums = nums;
-            st.resize(4* size);
-            build(0, 0, size-1);
-        }
-
-        bool query(int idx, int left, int right, int num){
-            if(st[idx] < num) return false;
-            if(left == right) {
-                st[idx]=0;
-                return true;
-            }
-
-            bool rv=query(idx*2+1, left, (left+right)/2, num);
-            if(!rv) rv= query(idx*2+2, (left+right)/2+1, right, num);
-            st[idx]= max(st[idx*2+1], st[idx*2+2]);
-            // cout<<idx<<" "<<st[idx]<<endl;
-            return rv;
-        }
-    };
-
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        segmentTree* s= new segmentTree(baskets);
         int ans=0;
         for(int i: fruits){
-            if(!s->query(0, 0, baskets.size()-1, i)){
-                ans++;
+            bool possible = true;
+            for(int& j: baskets){
+                if(i <= j){
+                    j=0;
+                    possible= false;
+                    break;
+                }
             }
-            // cout<<endl<<endl;
+            if(possible) ans++;
         }
         return ans;
     }
