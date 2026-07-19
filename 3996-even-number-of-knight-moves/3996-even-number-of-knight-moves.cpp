7e9c37becfa88c98aedb0656d7bf55d489ca3c6f
Atulkumar58 <atulchaudhary151@gmail.com>
2026-07-19 23:53:19 +0530

    Time: 0 ms (100%), Space: 49.8 MB (13.5%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
-    int dx[8]= {2, -2, 2, -2, 1, 1, -1, -1};
-    int dy[8]= {1, 1, -1, -1, 2, -2, 2, -2};
-    unordered_map<int, bool> m;
-    vector<int>target;
-    bool check(int a, int b, bool toggle){
-        if(a== target[0] && b== target[1] && toggle) return 1;
-        if(a<0 || a>=8 ||b<0 || b>=8) return 0;
-        // if(toggle){
-            if(m[a*10 + b]) return false;
-            m[a*10 + b] = 1;
-            // }
-        // cout<<a<<" "<<b<<endl;
-        for(int i=0; i<8; i++){
-            if(check(a+dx[i], b+dy[i], !toggle)) return 1;
-        }
-        return 0;
-    }
     bool canReach(vector<int>& start, vector<int>& target) {
-        this->target= target;
-        m.clear();
-        return check(start[0], start[1], 1);
+        int a= abs(start[1]-start[0]);
+        int b= abs(target[1]-target[0]);
+        if(a%2 == b%2) return 1;
+        return 0;
     }
 };
