498444e625adf31b83575f780da0263777e6627e
Atulkumar58 <atulchaudhary151@gmail.com>
2026-07-19 23:35:38 +0530

    Time: 14 ms (5.04%), Space: 53.8 MB (5.38%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -7,10 +7,10 @@ public:
     bool check(int a, int b, bool toggle){
         if(a== target[0] && b== target[1] && toggle) return 1;
         if(a<0 || a>=8 ||b<0 || b>=8) return 0;
-        if(toggle){
+        // if(toggle){
             if(m[a*10 + b]) return false;
             m[a*10 + b] = 1;
-            }
+            // }
         // cout<<a<<" "<<b<<endl;
         for(int i=0; i<8; i++){
             if(check(a+dx[i], b+dy[i], !toggle)) return 1;
