b06361460e01586c333db691160af2144790a0d3
Atulkumar58 <atulchaudhary151@gmail.com>
2026-08-10 12:09:30 +0530

    Time: 63 ms (35.42%), Space: 18.2 MB (23.56%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    vector<int>dp;
-    int rec(int n, bool turn){
-        if(n==0){
-            if(turn) return dp[0];
-            else return !dp[0];
-        }
-
-        if(dp[n] != -1){
-            if(turn) return dp[n];
+    vector<int>dp; //1 means person on that position of number wins
+                //  0 means person on that number loses
+    bool rec(int n, bool alice_turn){
+        if(n==0 || dp[n] != -1){
+            if(alice_turn) return dp[n];
             else return !dp[n];
         }
 
         for(int i=1; i*i<=n; i++){
-            if(turn){
-                if(rec(n-i*i, !turn)){
+            if(alice_turn){
+                if(rec(n-i*i, !alice_turn)){
                     dp[n]= 1;
-                    
                     return true;
                 }
             }
             else {
-                if(!rec(n-i*i, !turn)){
+                if(!rec(n-i*i, !alice_turn)){
                     dp[n]= 1;
                     return false;
                 }
             }
         }
-        if(turn){
+
+        //person didnot find any winning stretegy that is why loses on both cases dp-> 0 
+        // we return the value with respect to winning of alice which is asked in the question
+        if(alice_turn){
             dp[n]=0;
             return false;
-        } 
-        dp[n]= 0;
-        return 1;
+        }
+        else{
+            dp[n]= 0;
+            return 1;
+        }
     }
     bool winnerSquareGame(int n) {
         dp.resize(n+1, -1);
         dp[0]=0;
-        
-        
-        // for(int i: dp)cout<<i<<" ";
         return rec(n, 1);
     }
 };
