c58c7dffdc7688fbf4c9f48f485b9a60af83bff1
Atulkumar58 <atulchaudhary151@gmail.com>
2026-08-16 23:01:28 +0530

    Time: 350 ms (90.2%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,10 +1,6 @@
 select
 q.query_name,
 round(avg(rating/position), 2) as quality,
-round(q2.poor_query_percentage*100, 2) as poor_query_percentage
+round(sum(rating < 3)*100/count(*), 2) as poor_query_percentage
 From Queries q
-join (select query_name, sum(rating < 3)/count(*) as poor_query_percentage
-from Queries
-Group by query_name) q2
-ON q.query_name = q2.query_name
 group by query_name;
