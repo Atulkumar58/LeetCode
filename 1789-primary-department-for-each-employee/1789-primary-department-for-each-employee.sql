77216e93680ab0334533bb4468093b8b7c872000
Atulkumar58 <atulchaudhary151@gmail.com>
2026-08-18 23:16:00 +0530

    Time: 5584 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+select employee_id,
+department_id
+from Employee
+where primary_flag="Y" 
+or employee_id in 
+(
+    select employee_id
+    from Employee
+    Group by employee_id
+    having count(*) =1
+);
