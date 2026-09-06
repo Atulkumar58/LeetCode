c6c5fcea14fec8909a78584d18cc8821dbeb27fe
Atul Kumar <atulchaudhary151@gmail.com>
2026-09-06 16:48:55 +0530

    Time: 475 ms (72.28%), Space: 245.2 MB (15.69%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,54 +1,48 @@
 class Solution {
 public:
-    int ans=0;
+    int ans;
     vector<int> nums;
     void conquer(int i, int j){
-        vector<int> x;
         int mid= (i+j)/2;
-        int s1=i, s2= (i+j)/2+1;
-        while(s1 <= mid && s2 <= j){
-            if((long long)nums[s1] <= (long long)nums[s2]*2){
-                s1++;
-            }else{
-                ans+= (mid -s1+1);
-                s2++;
-            }
-        }
 
-        s1=i, s2= mid+1;
-        while(s1 <= mid && s2 <= j){
-            if(nums[s1] <= nums[s2]){
-                x.push_back(nums[s1++]);
-            }else{
-                x.push_back(nums[s2++]);
-            }
-        }
+        int x= i, y= mid+1;
 
-        while(s1 <= mid) x.push_back(nums[s1++]);
-        while(s2 <= j) x.push_back(nums[s2++]);
-        // cout<<i<<" "<<j<<endl;
-        for(int idx=0; idx<x.size(); idx++){
-            nums[idx + i] = x[idx];
-            // cout<<nums[idx+i]<<" ";
+        while(x<=mid && y<= j){
+            if((long long)nums[x] > (long long)nums[y]*2 ){
+                ans+= (mid-x+1);
+                y++;
+            }
+            else x++;
+        }
+        x=i, y=mid+1;
+        // for(int idx=i; idx<=j; idx++) cout<<nums[idx]; cout<<endl;
+        vector<int> temp;
+        while(x<=mid && y<= j){
+            if(nums[x] <= nums[y]){
+                temp.push_back(nums[x++]);
+            }
+            else temp.push_back(nums[y++]);
+        }
+        while(x<=mid) temp.push_back(nums[x++]);
+        while(y<=j) temp.push_back(nums[y++]);
+
+        for(int x= i; x<=j; x++){
+            nums[x]= temp[x-i];
+            // cout<<nums[x]<<" ";
         }
         // cout<<i<<" "<<j<<" "<<ans<<endl;
-        // cout<<endl;
     }
-    void divide( int i, int j){
-        if(i == j) return;
-
+    void divide(int i, int j){
+        if(i >= j) return;
         int mid= (i+j)/2;
         divide(i, mid);
         divide(mid+1, j);
-
         conquer(i, j);
     }
     int reversePairs(vector<int>& nums) {
         ans=0;
-        int n= nums.size();
-        this->nums = nums;
-        divide(0, n-1);
-        // for(int i: nums) cout<<i<<" ";
+        this->nums= nums;
+        divide(0, nums.size()-1);
         return ans;
     }
 };
