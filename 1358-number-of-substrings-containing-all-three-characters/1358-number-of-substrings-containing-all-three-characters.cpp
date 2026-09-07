f146a41bd09e5458d76e45f1d8119bc938c94f4d
Atul Kumar <atulchaudhary151@gmail.com>
2026-09-07 12:10:50 +0530

    Time: 10 ms (46.6%), Space: 11.1 MB (33.37%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
+    bool check(vector<int> &arr){
+        if(arr[0]>0 && arr[1]>0 && arr[2]>0)
+        return true;
+        return false;
+    }
+    void enter(vector<int>& arr, char s){
+        switch(s){
+            case 'a': arr[0]++;
+                break;
+            case 'b': arr[1]++;
+                break;
+            case 'c': arr[2]++;
+                break;
+        }
+    }
+    void del(vector<int>& arr, char s){
+        switch(s){
+            case 'a': arr[0]--;
+                break;
+            case 'b': arr[1]--;
+                break;
+            case 'c': arr[2]--;
+                break;
+        }
+    }
     int numberOfSubstrings(string s) {
+        vector<int> arr(3, 0);
         int n= s.length();
-
-        int i=0, j=0;
-        vector<int> count(3, 0);
+        int j=0;
         int ans=0;
-        while(j < n){
-            if(count[0] && count[1] && count[2]){
-                ans+= (n- (j)+1);
-                count[s[i]-'a']--;
-                i++;
-            }else{
-                count[s[j]-'a']++;
+        for(int i=0; i<n; i++){
+            while(!check(arr) && j<n){
+                enter(arr, s[j]);
                 j++;
             }
-            // cout<< i << " "<<j<<" "<<ans<<endl;
+            if(check(arr))
+            ans+= (n-j+1);
+            // cout<<ans<<" "<<i<< " "<<j<<endl;
+            del(arr, s[i]);
         }
-        while(count[0] && count[1] && count[2]){
-                ans+= (n- (j)+1);
-                count[s[i]-'a']--;
-                i++;
-            }
         return ans;
     }
+    
 };
