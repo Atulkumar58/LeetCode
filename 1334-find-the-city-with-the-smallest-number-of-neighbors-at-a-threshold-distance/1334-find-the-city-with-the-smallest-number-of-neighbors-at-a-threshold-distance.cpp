9eab87e34bbd7b183289532c3a5dbbf7f84c725f
Atul Kumar <atulchaudhary151@gmail.com>
2026-09-24 16:35:39 +0530

    Time: 27 ms (15.94%), Space: 19.8 MB (22.02%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,56 +1,46 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> distance(n, vector<int>(n, distanceThreshold+1));
-        unordered_map<int, vector<int>> adj;
-        for(auto& i: edges){
-            if(i[2] > distanceThreshold) continue;
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-            distance[i[0]][i[1]] = i[2];
-            distance[i[1]][i[0]] = i[2];
-        }
+    vector<vector<pair<int, int>>> graph;
+    int countNeighbour(int city, int threshold){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>pq;
+        vector<int> dist(graph.size(), INT_MAX);
+        pq.push({0, city});
+        dist[city]=0;
+        while(!pq.empty()){
+            pair<int, int> p= pq.top();
+            pq.pop();
 
-        vector<bool> changed(n , true);
-        bool change= 1;
-        queue<int>q;
-        for(int i=0; i<n; i++) q.push(i);
-
-        while(!q.empty()){
-            int i= q.front();
-            q.pop();
-            changed[i]=0;
-            
-                    for(int j: adj[i]){
-                        for(int k=0; k<n; k++){
-                            int temp = (long long)distance[i][k] + distance[i][j];
-                            if(temp < distance[j][k] && temp <= distanceThreshold){
-                                distance[j][k]= distance[k][j] = distance[i][k] + distance[i][j];
-                                if(!changed[k])q.push(k);
-                                if(!changed[j])q.push(j);
-                                changed[k]= changed[j]= 1;
-                            }
-                        }
-                    }
-        }
-        int sum= INT_MAX;
-        int ans=0;
-        for(int i=0; i<n; i++){
-            int c=0;
-            for(int j=0; j<n; j++){
-                // cout<<distance[i][j]<<" ";
-                if(i== j)continue;
-                if(distance[i][j] <= distanceThreshold){
-                    c++;
+            for(auto& i: graph[p.second]){
+                if(dist[i.first] > p.first + i.second && p.first+i.second <= threshold){
+                    dist[i.first] = p.first + i.second;
+                    pq.push({dist[i.first], i.first});
                 }
             }
-            // cout<<endl;
-            // cout<<c<<endl;
-            if(sum >= c) {
-                sum= c;
-                ans= i;
-            }
+        }
+
+        int ans=0;
+        for(int i: dist) {
+            if(i!= INT_MAX) ans++;
         }
         return ans;
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        graph.clear();
+        graph.resize(n);
+        for(auto& i: edges){
+            graph[i[0]].push_back({i[1], i[2]});
+            graph[i[1]].push_back({i[0], i[2]});
+        }
+        int ans=INT_MAX;
+        int city=-1;
+        for(int i=0; i<n; i++){
+            int t= countNeighbour(i, distanceThreshold);
+            cout<<t<<" ";
+            if(t <= ans){
+                ans= t;
+                city= i;
+            }
+        }
+        return city;
     }
 };
